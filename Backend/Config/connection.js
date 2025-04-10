9810c3474f2fafb706ae2ef9fc4d04d972d16bff
UdayBari3421 <udaybari2003@gmail.com>
2025-04-10 22:01:56 +0530

    bug fix

--- Backend/Config/connection.js
+++ Backend/Config/connection.js
@@ -8,7 +8,7 @@ export const connectDb = async () => {
       console.log("Connected to MongoDB");
     });
     retries++;
-    await mongoose.connect(process.env.DEV_MONGO_URI || `${process.env.PROD_MONGO_URI}/blogApp`);
+    await mongoose.connect(`${process.env.PROD_MONGO_URI}/blogApp`);
   } catch (error) {
     console.log(`Failed to connect to MongoDB: ${error}`);
     if (retries < Number(process.env.DB_RETRIES)) {
