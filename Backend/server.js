d036f6dcae15870f907f38b02de45bf5bc5585d2
UdayBari3421 <udaybari2003@gmail.com>
2025-04-11 21:20:44 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,15 @@ connectDb();
 
 const app = express();
 
+const corsConfig = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsConfig));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter);
